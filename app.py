d2acc49c31ffada3617a7d622518cd23ea316935
Raul Munoz <munozzraul30@gmail.com>
2025-05-31 00:20:58 +0200

    Forzar actualización y limpiar import innecesario

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, session, url_for
 from flask_sqlalchemy import SQLAlchemy
 import json
+# Cambio mínimo para forzar commit
 
 
 app = Flask(__name__)
