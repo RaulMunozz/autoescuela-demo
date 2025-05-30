9f49375cb4f467e8e5b8d18f3aea06de9bd67e8c
Raul Munoz <munozzraul30@gmail.com>
2025-05-31 00:11:07 +0200

    Eliminar script temporal de borrado

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, url_for
 from flask_sqlalchemy import SQLAlchemy
 import json
-import borrar_db_remoto
 
 
 app = Flask(__name__)
--- borrar_db_remoto.py
+++
@@ -1,7 +0,0 @@
-import os
-
-try:
-    os.remove("disponibilidad.db")
-    print("✅ Base de datos borrada correctamente en Render.")
-except Exception as e:
-    print("❌ No se pudo borrar la base de datos:", e)
